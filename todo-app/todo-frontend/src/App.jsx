3d3dda0cd1d1dcd0e75fb1ec1dfefb60f70556fa
Kizuna Akari <lecion20@outlook.com>
2024-05-26 22:06:35 +0800

    12.15 good solution for hot reload from https://patrickdesjardins.com/blog/docker-vitejs-hot-reload

--- todo-app/todo-frontend/src/App.jsx
+++ todo-app/todo-frontend/src/App.jsx
@@ -4,6 +4,10 @@ import TodoView from './Todos/TodoView'
 function App() {
   return (
     <div className="App">
+      <div>docker run -p 5173:5173 -it -v "$(pwd):/usr/src/app/" hello-front-dev</div>
+      <div>This works weirdly with an interactive window of containerized vite.</div>
+      <div>This is the vite version of Todos.</div>
+      <div>Aha good tutorial to get this working from https://patrickdesjardins.com/blog/docker-vitejs-hot-reload</div>
       <TodoView />
     </div>
   );
