310c1ddea456336075719aa860e37a5894b00421
Kizuna Akari <lecion20@outlook.com>
2024-06-01 20:39:02 +0800

    12.22

--- dockerBloglist/bloglist-frontend/src/index.js
+++ dockerBloglist/bloglist-frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { combineReducers, createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import notificationReducer from './reducer/notificationReducer';
